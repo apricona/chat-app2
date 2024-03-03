5c7a68c184cf192d989baffebb70428e5a78600a
apricona <anzu.sd.42@gmail.com>
2024-03-03 14:21:40 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,11 +15,11 @@ class MessagesController < ApplicationController
       render :index, status: :unprocessable_entity
     end
   end
-  
+
   private
 
   def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
 end
